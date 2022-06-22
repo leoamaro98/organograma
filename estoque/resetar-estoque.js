33be5d96309b4f18c8fec73b7a31e31ad8035840
leozinhobk <43849758+leozinhobk@users.noreply.github.com>
2022-06-22 13:27:59 -0300

    Delete estoque directory

--- estoque/resetar-estoque.js
+++
@@ -1,8 +0,0 @@
-var btReset = document.querySelector('#reset-table');
-var tabble = document.querySelector('#tabela');
-let trs = document.querySelector(".produto");
-
-
-btReset.addEventListener('click', function () {
-   window.location.href = "homepage.html";
-})
